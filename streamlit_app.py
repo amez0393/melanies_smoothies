4cfc35b9a6d2390e973049701470e9b69ac8ec32
amez0393 <47357764+amez0393@users.noreply.github.com>
2025-02-07 19:00:03 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -31,7 +31,10 @@ if ingredients_list:
             values ('""" + ingredients_string + """','"""+name_on_order+"""'  )"""
    st.write(my_insert_stmt)
    #st.stop()
-   time_to_insert=st.button('Submit Order') 
+   time_to_insert=st.button('Submit Order')
+   import requests
+   smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+   st.text(smoothiefroot_response)
    if time_to_insert:
        session.sql(my_insert_stmt).collect()
        st.success('Your Smoothie is ordered!', icon="✅") 
