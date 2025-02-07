fff511c638b079f3abcad1cf53a5a9376afbf044
amez0393 <47357764+amez0393@users.noreply.github.com>
2025-02-07 19:03:12 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -23,6 +23,9 @@ ingredients_list=st.multiselect(
     , my_dataframe
     ,max_selections=5
 )
+import requests
+smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+st.text(smoothiefroot_response)
 if ingredients_list:
    ingredients_string=''
    for fruit_chosen in ingredients_list:
@@ -35,7 +38,4 @@ if ingredients_list:
    if time_to_insert:
        session.sql(my_insert_stmt).collect()
        st.success('Your Smoothie is ordered!', icon="✅") 
-       import requests
-       smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-       st.text(smoothiefroot_response)
    st.stop()
